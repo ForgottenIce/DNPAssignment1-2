ddd25e0f42dd854bc3191a8ff67f3a055a5c88f1
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2022-10-30 18:07:01 +0100

    Radzen library import
    Radzen library imported and test components added to index.razor

--- BlazorWASM/Program.cs
+++ BlazorWASM/Program.cs
@@ -1,10 +1,17 @@
 using BlazorWASM;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//Radzen services
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
