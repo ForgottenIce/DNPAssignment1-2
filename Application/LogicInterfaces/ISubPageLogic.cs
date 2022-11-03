daa82fd27c22037bb65fa809366407d5a4112265
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2022-11-03 01:02:40 +0100

    Update ISubPageLogic.cs
    Removed todo comment

--- Application/LogicInterfaces/ISubPageLogic.cs
+++ Application/LogicInterfaces/ISubPageLogic.cs
@@ -4,7 +4,7 @@ using Domain.Models;
 namespace Application.LogicInterfaces;
 
 public interface ISubPageLogic {
-    Task<SubPage> CreateAsync(SubPageCreationDto subPageToCreate); //Todo rename method
+    Task<SubPage> CreateAsync(SubPageCreationDto subPageToCreate);
     Task<SubPage> GetByIdAsync(string id);
     Task<IEnumerable<SubPage>> GetAsync();
     Task<IEnumerable<Post>> GetPostsAsync(string subPageId);
