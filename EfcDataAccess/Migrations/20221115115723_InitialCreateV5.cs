b252f17a70159e9db99b086f90ae4d460258003e
ForgottenIce <38312721+ForgottenIce@users.noreply.github.com>
2022-11-16 12:05:47 +0100

    Database and Migrations
    - Created EFC Migrations from the Domain.
    - Generated database from the migrations

---
+++ EfcDataAccess/Migrations/20221115115723_InitialCreateV5.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfcDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreateV5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    HashedPassword = table.Column<string>(type: "TEXT", nullable: false),
+                    Karma = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubPages",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    OwnerId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubPages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubPages_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Body = table.Column<string>(type: "TEXT", nullable: false),
+                    AuthorId = table.Column<string>(type: "TEXT", nullable: false),
+                    Likes = table.Column<int>(type: "INTEGER", nullable: false),
+                    Dislikes = table.Column<int>(type: "INTEGER", nullable: false),
+                    PostId = table.Column<string>(type: "TEXT", nullable: true),
+                    SubPageId = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Posts_SubPages_SubPageId",
+                        column: x => x.SubPageId,
+                        principalTable: "SubPages",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserDislikesPost",
+                columns: table => new
+                {
+                    DislikedPostsId = table.Column<string>(type: "TEXT", nullable: false),
+                    User1Id = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDislikesPost", x => new { x.DislikedPostsId, x.User1Id });
+                    table.ForeignKey(
+                        name: "FK_UserDislikesPost_Posts_DislikedPostsId",
+                        column: x => x.DislikedPostsId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserDislikesPost_Users_User1Id",
+                        column: x => x.User1Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserLikesPost",
+                columns: table => new
+                {
+                    LikedPostsId = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserLikesPost", x => new { x.LikedPostsId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserLikesPost_Posts_LikedPostsId",
+                        column: x => x.LikedPostsId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserLikesPost_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorId",
+                table: "Posts",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_PostId",
+                table: "Posts",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_SubPageId",
+                table: "Posts",
+                column: "SubPageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubPages_OwnerId",
+                table: "SubPages",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDislikesPost_User1Id",
+                table: "UserDislikesPost",
+                column: "User1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserLikesPost_UserId",
+                table: "UserLikesPost",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDislikesPost");
+
+            migrationBuilder.DropTable(
+                name: "UserLikesPost");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "SubPages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ EfcDataAccess/Migrations/20221115131907_CommentTitleCanNowBeNull.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfcDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentTitleCanNowBeNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubPages_Users_OwnerId",
+                table: "SubPages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "SubPages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateTable(
+                name: "UserSubscribedToSub",
+                columns: table => new
+                {
+                    SubscribedSubsId = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSubscribedToSub", x => new { x.SubscribedSubsId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_UserSubscribedToSub_SubPages_SubscribedSubsId",
+                        column: x => x.SubscribedSubsId,
+                        principalTable: "SubPages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserSubscribedToSub_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSubscribedToSub_UserId",
+                table: "UserSubscribedToSub",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubPages_Users_OwnerId",
+                table: "SubPages",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubPages_Users_OwnerId",
+                table: "SubPages");
+
+            migrationBuilder.DropTable(
+                name: "UserSubscribedToSub");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OwnerId",
+                table: "SubPages",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubPages_Users_OwnerId",
+                table: "SubPages",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+    }
+}
